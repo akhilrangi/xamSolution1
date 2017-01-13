3fced388c8bd05c245b8bb4e95821fd25e3421a8
akhilrangi <akhil.rangi@gmail.com>
2017-01-13 13:43:00 +0000

    wip1

--- And1/Resources/Resource.Designer.cs
+++ And1/Resources/Resource.Designer.cs
@@ -60,8 +60,14 @@ namespace And1
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int buttonNext = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int buttonPrev = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView1 = 2131034114;
 			
 			static Id()
 			{
